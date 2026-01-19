a12d722b6908c545374204b1f6c616d14670647a
Tarcisio <tarcisioaraujoads@gmail.com>
2026-01-19 13:14:13 -0300

    add: página de sucesso pós-pagamento

---
+++ gerar-hash.js
@@ -0,0 +1,10 @@
+﻿const bcrypt = require('bcryptjs');
+const senha = '123456';
+
+bcrypt.hash(senha, 10, (err, hash) => {
+  if (err) {
+    console.log('Erro:', err);
+  } else {
+    console.log('Hash correto:', hash);
+  }
+});
---
+++ test-bcrypt.js
@@ -0,0 +1,11 @@
+﻿const bcrypt = require('bcryptjs');
+const hash = '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy';
+const senha = '123456';
+
+bcrypt.compare(senha, hash, (err, result) => {
+  if (err) {
+    console.log('Erro:', err);
+  } else {
+    console.log('Senha match:', result);
+  }
+});
