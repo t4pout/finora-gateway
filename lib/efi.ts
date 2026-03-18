97d4532d3b63af0d7713adbd442934a27fed76bf
Tarcisio <tarcisioaraujoads@gmail.com>
2026-03-18 04:31:13 -0300

    fix: certificado Efi recriado a cada instancia

--- lib/efi.ts
+++ lib/efi.ts
@@ -3,33 +3,33 @@ import path from 'path';
 import fs from 'fs';
 import os from 'os';
 
-let certificatePath: string;
+function getEfiPay() {
+  let certificatePath: string;
 
-const certBase64 = process.env.EFI_CERT_BASE64;
+  const certBase64 = process.env.EFI_CERT_BASE64;
 
-if (certBase64) {
-  const cleaned = certBase64
-    .replace(/-----BEGIN CERTIFICATE-----/g, '')
-    .replace(/-----END CERTIFICATE-----/g, '')
-    .replace(/\r\n/g, '')
-    .replace(/\n/g, '')
-    .replace(/\r/g, '')
-    .trim();
-  const certBuffer = Buffer.from(cleaned, 'base64');
-  const tempPath = path.join(os.tmpdir(), 'efi_cert.p12');
-  fs.writeFileSync(tempPath, certBuffer);
-  certificatePath = tempPath;
-} else {
-  certificatePath = path.resolve(process.cwd(), 'certs/producao-886484-finora payments.p12');
+  if (certBase64) {
+    const cleaned = certBase64
+      .replace(/-----BEGIN CERTIFICATE-----/g, '')
+      .replace(/-----END CERTIFICATE-----/g, '')
+      .replace(/\r\n/g, '')
+      .replace(/\n/g, '')
+      .replace(/\r/g, '')
+      .trim();
+    const certBuffer = Buffer.from(cleaned, 'base64');
+    const tempPath = path.join(os.tmpdir(), 'efi_cert_' + Date.now() + '.p12');
+    fs.writeFileSync(tempPath, certBuffer);
+    certificatePath = tempPath;
+  } else {
+    certificatePath = path.resolve(process.cwd(), 'certs/producao-886484-finora payments.p12');
+  }
+
+  return new EfiPay({
+    client_id: process.env.EFI_CLIENT_ID!,
+    client_secret: process.env.EFI_CLIENT_SECRET!,
+    sandbox: process.env.EFI_SANDBOX === 'true',
+    certificate: certificatePath,
+  });
 }
 
-const options = {
-  client_id: process.env.EFI_CLIENT_ID!,
-  client_secret: process.env.EFI_CLIENT_SECRET!,
-  sandbox: process.env.EFI_SANDBOX === 'true',
-  certificate: certificatePath,
-};
-
-const efipay = new EfiPay(options);
-
-export default efipay;
+export default getEfiPay();
