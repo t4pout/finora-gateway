cd56c7cf63a3e3e69374568000d8083304af6ec9
Tarcisio <tarcisioaraujoads@gmail.com>
2026-03-17 22:53:45 -0300

    feat: notification_url no boleto Efi

--- app/api/efi/boleto/route.ts
+++ app/api/efi/boleto/route.ts
@@ -33,6 +33,7 @@ export async function POST(req: NextRequest) {
             },
           },
           expire_at: expireAt,
+          notification_url: 'https://www.finorapayments.com/api/efi/webhook',
         },
       },
     };
