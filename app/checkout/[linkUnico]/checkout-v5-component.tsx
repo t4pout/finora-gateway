f331958197ea9d9f48443950da787bd4d45cb942
Tarcisio <tarcisioaraujoads@gmail.com>
2026-07-14 17:38:14 -0300

    fix: adiciona placeholder do checkout-v5-component para destravar build

---
+++ app/checkout/[linkUnico]/checkout-v5-component.tsx
@@ -0,0 +1,6 @@
+'use client';
+import CheckoutV2Component from './checkout-v2-component';
+
+export default function CheckoutV5Component(props: any) {
+  return <CheckoutV2Component {...props} />;
+}
