f2be0f5d3bb4d5c72a7572527546bdb0979b83ff
Tarcisio <tarcisioaraujoads@gmail.com>
2026-07-27 00:09:14 -0300

    fix: adiciona checkoutQuantidadeInicial na rota PATCH de planos (campo novo nao estava sendo salvo)

--- app/api/planos/[id]/route.ts
+++ app/api/planos/[id]/route.ts
@@ -69,6 +69,7 @@ export async function PATCH(
     if (body.checkoutBackRedirect !== undefined) data.checkoutBackRedirect = body.checkoutBackRedirect;
     if (body.checkoutBackRedirectUrl !== undefined) data.checkoutBackRedirectUrl = body.checkoutBackRedirectUrl;
     if (body.checkoutCondicaoDesconto !== undefined) data.checkoutCondicaoDesconto = body.checkoutCondicaoDesconto;
+    if (body.checkoutQuantidadeInicial !== undefined) data.checkoutQuantidadeInicial = body.checkoutQuantidadeInicial;
     if (body.checkoutPadBanner !== undefined) data.checkoutPadBanner = body.checkoutPadBanner;
     if (body.checkoutPadLogoSuperior !== undefined) data.checkoutPadLogoSuperior = body.checkoutPadLogoSuperior;
     if (body.checkoutPadLogoInferior !== undefined) data.checkoutPadLogoInferior = body.checkoutPadLogoInferior;
